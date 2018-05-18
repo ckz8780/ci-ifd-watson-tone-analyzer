2b3ab6cd2f95ec851fd8287a2f4c3502b8276444
Chris Zielinski <ckz8780@gmail.com>
2018-05-18 20:34:39 +0000

    Added basic document tone detection

--- js/watson.js
+++ js/watson.js
@@ -1,4 +1,46 @@
 function analyze() {
     var textToAnalyze = $('#analysis-text').val();
-    $('#results').html($('#analysis-text').val());
+    
+    $.ajax({
+        type: 'POST',
+        url: 'https://cors-anywhere.herokuapp.com/https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2017-09-21',
+        headers: {
+            'Content-Type': 'text/plain;charset=utf-8',
+            'Accept': 'application/json',
+            'Content-Language': 'en',
+            'Accept-Language': 'en',
+            'Authorization': 'Basic ' + btoa('27ed9380-b340-408c-b554-7057f3beb6f8:YNBdZB8Xrjmk')
+        },
+        data: textToAnalyze
+    }).done(function(data) {
+        var docTones = data.document_tone.tones;
+        var sentTones = data.sentences_tone;
+        var docToneTableInnerHTML = ''
+        
+        docTones.forEach(function(docTone) {
+            console.log(docTone);
+            docToneTableInnerHTML += `
+                                    <tr>
+                                        <td class="doctone-name">${docTone.tone_name}</td>
+                                        <td class="doctone-score">${docTone.score}</td>
+                                    </tr>`;
+        });
+        
+        sentTones.forEach(function(sentTone) {
+            
+            console.log(sentTone);
+            var sentId = sentTone.sentence_id;
+            var sentText = sentTone.text;
+            var sentTones = sentTone.tones.length;
+            
+            console.log('Sentence ' + sentId + ': ' + sentText + ' (' + sentTones + ' different tones detected)');
+        });
+        
+        var docHTML = `<h2 class="tone-header">Document Tone:</h2><table class="table table-sm table-striped table-hover"><tbody><tr><th>Tone Name</th><th>Score</th></tr>${docToneTableInnerHTML}</tbody></table>`;
+        var sentHTML = `<h2 class="tone-header">Sentence Tone:</h2><table class="table table-sm table-striped table-hover"></table>`;
+        
+        $('#doc-results').html(docHTML);
+        $('#sent-results').html(sentHTML);
+        
+    });
 }
