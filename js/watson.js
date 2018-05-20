0c6960eb47298e9afa490a883dc0fc70f3723f63
Chris Zielinski <ckz8780@gmail.com>
2018-05-20 03:16:09 +0000

    added conditional logic for styling scores/tones

--- js/watson.js
+++ js/watson.js
@@ -19,10 +19,35 @@ function analyze() {
         var sentToneTableInnerHTML = '';
         
         docTones.forEach(function(docTone) {
+            if(docTone.tone_name == 'Anger') {
+                var className = 'tone-anger';
+            } else if(docTone.tone_name == 'Fear') {
+                var className = 'tone-fear';
+            } else if(docTone.tone_name == 'Joy') {
+                var className = 'tone-joy';
+            } else if(docTone.tone_name == 'Sadness') {
+                var className = 'tone-sadness';
+            } else if(docTone.tone_name == 'Analytical') {
+                var className = 'tone-analytical';
+            } else if(docTone.tone_name == 'Confident') {
+                var className = 'tone-confident';
+            } else if(docTone.tone_name == 'Tentative') {
+                var className = 'tone-tentative';
+            } else {
+                var className = 'tone-unknown';
+            }
+            
+            if(docTone.score >= 0 && docTone.score <= 0.4999) {
+                var scoreClassName = 'score-low';
+            } else if(docTone.score >= 0.5 && docTone.score <= 0.7499) {
+                var scoreClassName = 'score-med';
+            } else if(docTone.score >= 0.75 && docTone.score <= 1.0) {
+                var scoreClassName = 'score-high';
+            }
             docToneTableInnerHTML += 
                 `<tr>
-                    <td class="doctone-name">${docTone.tone_name}</td>
-                    <td class="doctone-score">${docTone.score}</td>
+                    <td class="doctone-name"><ul class="list-unstyled"><li class="doc-tone-name ${className}">${docTone.tone_name}</li></ul></td>
+                    <td class="doctone-score"><ul class="list-unstyled"><li class="doc-tone-score ${scoreClassName}">${(docTone.score*100).toFixed(2)}%</li></ul></td>
                 </tr>`;
         });
         
@@ -33,8 +58,33 @@ function analyze() {
                 var indToneScoresHTML = '<ul class="list-unstyled">';
                 
                 sentTone.tones.forEach(function(indTone) {
-                    indTonesHTML += `<li class="text-success">${indTone.tone_name}</li>`;
-                    indToneScoresHTML += `<li class="text-success">${indTone.score}</li>`;
+                    if(indTone.tone_name == 'Anger') {
+                        var className = 'tone-anger';
+                    } else if(indTone.tone_name == 'Fear') {
+                        var className = 'tone-fear';
+                    } else if(indTone.tone_name == 'Joy') {
+                        var className = 'tone-joy';
+                    } else if(indTone.tone_name == 'Sadness') {
+                        var className = 'tone-sadness';
+                    } else if(indTone.tone_name == 'Analytical') {
+                        var className = 'tone-analytical';
+                    } else if(indTone.tone_name == 'Confident') {
+                        var className = 'tone-confident';
+                    } else if(indTone.tone_name == 'Tentative') {
+                        var className = 'tone-tentative';
+                    } else {
+                        var className = 'tone-unknown';
+                    }
+                    
+                    if(indTone.score >= 0 && indTone.score <= 0.4999) {
+                        var scoreClassName = 'score-low';
+                    } else if(indTone.score >= 0.5 && indTone.score <= 0.7499) {
+                        var scoreClassName = 'score-med';
+                    } else if(indTone.score >= 0.75 && indTone.score <= 1.0) {
+                        var scoreClassName = 'score-high';
+                    }
+                    indTonesHTML += `<li class="sent-tone-name ${className}">${indTone.tone_name}</li>`;
+                    indToneScoresHTML += `<li class="sent-tone-score ${scoreClassName}">${(indTone.score*100).toFixed(2)}%</li>`;
                 });
                 
                 indTonesHTML += '</ul>';
@@ -45,7 +95,7 @@ function analyze() {
                         <td class="senttone-id">${sentTone.sentence_id}</td>
                         <td class="senttone-text">${sentTone.text}</td>
                         <td class="sentence-name">${indTonesHTML}</td>
-                        <td class="sentence-name">${indToneScoresHTML}</td>
+                        <td class="sentence-score">${indToneScoresHTML}</td>
                         <td class="senttone-count">${sentTone.tones.length}</td>
                     </tr>`;
             });
