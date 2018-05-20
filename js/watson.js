71293bdfeba03557833ffd3f39375730bcbe1bb9
Chris Zielinski <ckz8780@gmail.com>
2018-05-20 00:22:11 +0000

    Added basic error handling.

--- js/watson.js
+++ js/watson.js
@@ -13,14 +13,12 @@ function analyze() {
         },
         data: textToAnalyze
     }).done(function(data) {
-        console.log(data);
         var docTones = data.document_tone.tones;
         var sentTones = data.sentences_tone;
         var docToneTableInnerHTML = '';
         var sentToneTableInnerHTML = '';
         
         docTones.forEach(function(docTone) {
-            console.log(docTone);
             docToneTableInnerHTML += 
                 `<tr>
                     <td class="doctone-name">${docTone.tone_name}</td>
@@ -28,38 +26,40 @@ function analyze() {
                 </tr>`;
         });
         
-        sentTones.forEach(function(sentTone) {
-            
-            var indTonesHTML = '<ul class="list-unstyled">';
-            var indToneScoresHTML = '<ul class="list-unstyled">';
-            
-            sentTone.tones.forEach(function(indTone) {
-                indTonesHTML += `<li class="text-success">${indTone.tone_name}</li>`;
-                indToneScoresHTML += `<li class="text-success">${indTone.score}</li>`;
+        if(!(typeof(sentTones) == 'undefined')) {
+            sentTones.forEach(function(sentTone) {
+                
+                var indTonesHTML = '<ul class="list-unstyled">';
+                var indToneScoresHTML = '<ul class="list-unstyled">';
+                
+                sentTone.tones.forEach(function(indTone) {
+                    indTonesHTML += `<li class="text-success">${indTone.tone_name}</li>`;
+                    indToneScoresHTML += `<li class="text-success">${indTone.score}</li>`;
+                });
+                
+                indTonesHTML += '</ul>';
+                indToneScoresHTML += '</ul>';
+                
+                sentToneTableInnerHTML += 
+                    `<tr>
+                        <td class="senttone-id">${sentTone.sentence_id}</td>
+                        <td class="senttone-text">${sentTone.text}</td>
+                        <td class="sentence-name">${indTonesHTML}</td>
+                        <td class="sentence-name">${indToneScoresHTML}</td>
+                        <td class="senttone-count">${sentTone.tones.length}</td>
+                    </tr>`;
             });
-            
-            indTonesHTML += '</ul>';
-            indToneScoresHTML += '</ul>';
-            
-            sentToneTableInnerHTML += 
-                `<tr>
-                    <td class="senttone-id">${sentTone.sentence_id}</td>
-                    <td class="senttone-text">${sentTone.text}</td>
-                    <td class="sentence-name">${indTonesHTML}</td>
-                    <td class="sentence-name">${indToneScoresHTML}</td>
-                    <td class="senttone-count">${sentTone.tones.length}</td>
-                </tr>`;
-        });
-        
+            var sentHeaderHTML = `<h2 class="tone-header">Sentence Tone:</h2>`;
+            var sentHTML = `<table class="table"><tbody><tr><th>Sentence ID</th><th>Text</th><th>Tone Name</th><th>Score</th><th>Tone Count</th></tr>${sentToneTableInnerHTML}</tbody></table>`;
+            $('#sent-header').html(sentHeaderHTML);
+            $('#sent-results').html(sentHTML);
+        }
+
         var docHeaderHTML = `<h2 class="tone-header">Document Tone:</h2>`;
-        var sentHeaderHTML = `<h2 class="tone-header">Sentence Tone:</h2>`;
         var docHTML = `<table class="table"><tbody><tr><th>Tone Name</th><th>Score</th></tr>${docToneTableInnerHTML}</tbody></table>`;
-        var sentHTML = `<table class="table"><tbody><tr><th>Sentence ID</th><th>Text</th><th>Tone Name</th><th>Score</th><th>Tone Count</th></tr>${sentToneTableInnerHTML}</tbody></table>`;
         
         $('#doc-results').html(docHTML);
-        $('#sent-header').html(sentHeaderHTML);
         $('#doc-header').html(docHeaderHTML);
-        $('#sent-results').html(sentHTML);
         $('.results').fadeIn();
     });
 }
